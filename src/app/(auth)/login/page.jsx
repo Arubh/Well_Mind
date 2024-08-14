b549f7cbe38e52c312677502177566c07d6d0f04
Aditya <iit2022003@iiitl.ac.in>
2024-08-15 03:07:48 +0530

    responsiveness added

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -6,7 +6,7 @@ const LoginPage = () => {
   return (
     <div className='flex'>
       <Waves />
-      <div className="w-[30%] flex flex-col items-center mt-10">
+      <div className="lg:w-[30%] w-[50%] rounded-[15px] flex flex-col items-center mt-10 lg:static absolute left-[26%]">
         <LoginForm />
       </div>
     </div>
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -5,7 +5,7 @@ const RegisterPage = () => {
   return (
     <div className='flex'>
       <Waves />
-      <div className="w-[30%] flex flex-col items-center mt-10">
+      <div className="lg:w-[30%] w-[50%] rounded-[15px] flex flex-col items-center mt-10 lg:static absolute left-[26%]">
         <RegisterForm />
       </div>
     </div>
