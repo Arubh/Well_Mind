cb4339535395c12caf0a6cc28787dd95e7f65f58
Aditya <iit2022003@iiitl.ac.in>
2024-08-15 02:38:41 +0530

    login page updated

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,14 +1,13 @@
 import LoginForm from "@/components/LoginForm";
+import { Waves } from "@/components/ui/Waves";
 import { handleGoogleLogin } from "@/utils/action";
 
 const LoginPage = () => {
   return (
     <div className='flex'>
-      <div className='w-[70%] h-[670px] bg-[red]'>
-        jijo
-      </div>
+      <Waves />
       <div className="w-[30%] flex flex-col items-center mt-10">
-      <LoginForm />
+        <LoginForm />
       </div>
     </div>
   );
