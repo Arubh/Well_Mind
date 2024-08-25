fbb546603e0615a87fb94282d2ec800f0ecb50dd
Aditya <iit2022003@iiitl.ac.in>
2024-08-25 14:07:52 +0530

    env changes

--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -2,8 +2,8 @@
 import React, { useEffect, useState } from 'react';
 import NewsCard from '@/components/newsCard';
 
-const API_KEY=""
-const BASE_URL = `https://newsapi.org/v2/top-headlines?language=en&category=health&pageSize=6&apiKey=${process.env.NEXT_PUBLIC_API}`;
+const API_KEY="fec15366a25844f8bf372a05a96b734a"
+const BASE_URL = `https://newsapi.org/v2/top-headlines?language=en&category=health&pageSize=6&apiKey=${API_KEY}`;
 
 export default function News() {
   const [news, setNews] = useState([]);
@@ -17,12 +17,11 @@ export default function News() {
     const fetchNews = async (page = 1) => {
       setStatus('loading'); 
       try {
-        const response = await fetch(`${BASE_URL}&page=${page}`);
+        const response = await fetch(`${BASE_URL}&page=${page}`, { cache: "no-store" });
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Network response w as not ok');
         }
         const data = await response.json();
-        console.log(data)
         setNews(data.articles);
         setTotalResults(data.totalResults);
         setStatus('succeeded');
